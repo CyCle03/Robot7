2b690b802d6cf206cac1ae3d7e7f7cad1036e949
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-03 14:48:54 +0900

    Add quiz07.c to print multiplication table and letters
    This program prints the multiplication table for 3 and displays uppercase letters A-Z followed by lowercase letters a-z.

---
+++ C/Basic/Loop/quiz07.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	//1. 3단을 출력해 주세요. 
+	for (int i = 1; i < 10; i++)
+	{
+		printf("3 * %d = %d\n", i, (3 * i));
+	}
+
+  printf("\n");
+  
+	//2. 대문자 A~Z까지 소문자 a ~z까지 출력해주세요.
+	char large = 'A';//char 대신 int로 해도 된다.
+	char small = 'a';
+  
+	for (large = 'A'; large <= 'Z'; large++)
+	{
+		printf("%c ", large);
+	}
+
+	printf("\n");
+	
+	for (small = 'a'; small <= 'z'; small++)
+	{
+		printf("%c ", small);
+	}
+
+	return 0;
+}
