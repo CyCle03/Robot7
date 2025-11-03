2ff594db230e589faf2a77f8aaf34e04f9e12506
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-03 11:01:58 +0900

    Add ex3-6.c to demonstrate string usage

---
+++ C/Chap03/ex3-6.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	char fruit[20] = "strawberry";
+
+	printf("딸기 : %s\n" , fruit);
+	printf("딸기잼 : %s %s\n", fruit, "jam");
+
+	return 0;
+}
