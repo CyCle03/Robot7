5ef5bd65e9b6d4baec4a5a23f48c616bf67cbff1
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-03 12:54:52 +0900

    Correct sum calculation in Loopback.c
    Fix for loop to correctly calculate the sum from 1 to 100.

--- C/Chap06/Loopback.c
+++ C/Chap06/Loopback.c
@@ -6,7 +6,7 @@ int main()
 	//1~100 더하면 누적합이 5050이 됩니다. 이를 for문으로 작성.
 	int sum = 0;
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 100; i++)//for(int i = 1; i <= 100 ; i++) sum += i;
 	{
 		sum += i + 1;
 	}
