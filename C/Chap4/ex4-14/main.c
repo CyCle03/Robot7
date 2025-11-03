34cb0f80069b0f5d282e1760cb2b15c930d2b99d
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-03 11:12:58 +0900

    Add files via upload

---
+++ C/Chap4/ex4-14/main.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a = 10, b = 5;
+	int result;
+
+	result = a / b * 2;
+	printf("result = %d\n", result);
+	result = ++a * 3;
+	printf("result = %d\n", result);
+	result = a > b && a != 5;
+	printf("result = %d\n", result);
+	result = a % 3 == 0;
+	printf("result = %d\n", result);
+
+	return 0;
+}
