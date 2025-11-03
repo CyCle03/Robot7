b35e13f3914f4c147dcb018904737efadcd5108d
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-03 11:58:17 +0900

    Add while loop to print numbers from 10 to 1
    Implement a while loop to print numbers from 10 to 1.

---
+++ C/Chap06/ex6-1-3.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	//10 ~ 1까지 출력하는 while을 작성하세요.
+	int i = 10;
+
+	while (i > 0)
+	{
+		printf("i : %d\n", i--);
+	}
+
+	return 0;
+}
