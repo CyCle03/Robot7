8709dbc20755531f76a8ecd68de2a7dd6f857abd
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-03 12:53:40 +0900

    Implement loop to calculate sum from 1 to 100
    Fix the sum calculation in the loop to correctly add numbers from 1 to 100.

---
+++ C/Chap06/Loopback.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	//1~100 더하면 누적합이 5050이 됩니다. 이를 for문으로 작성.
+	int sum = 0;
+
+	for (int i = 0; i < 100; i++)
+	{
+		sum += i + 1;
+	}
+	
+	printf("1 ~ 100의 누적 합 : %d\n", sum);
+
+	return 0;
+}
