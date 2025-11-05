6dc8d541d0d3ec40fca6e13ff0ad4b253ddabb8e
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-05 11:40:06 +0900

    Update quiz13-2.c

--- C/Quiz/quiz13-2.c
+++ C/Quiz/quiz13-2.c
@@ -1,10 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<limits.h>
 
 int main()
 {
 	int arr[6] = { 3, 7, 2, 9, 5, 1 };
-	int max = 0;
+	//int max = 0;
+	int max = INT_MIN;
 	int index = 0;
 
 	for (int i = 0; i < 6; i++)
