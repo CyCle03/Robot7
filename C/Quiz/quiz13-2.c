19023e0d0dbfd7552764236bafcf91023af634d1
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-05 11:23:13 +0900

    Create quiz13-2.c

---
+++ C/Quiz/quiz13-2.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int arr[6] = { 3, 7, 2, 9, 5, 1 };
+	int max = 0;
+	int index = 0;
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+			index = i;
+		}
+	}
+
+	printf("max : %d\nindex : %d\n", max, index);
+
+	return 0;
+}
