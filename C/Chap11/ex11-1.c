b02aab7b0739d304a7aebf772fb906436ffe56e6
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-06 15:25:07 +0900

    Update ex11-1.c

--- C/Chap11/ex11-1.c
+++ C/Chap11/ex11-1.c
@@ -10,7 +10,7 @@ int main()
 	{
 		small = cap + ('a' - 'A');
 	}
-	printf("대문자 : %c %c", cap, '\n');
+	printf("대문자 : %c %c", cap, '\n');//'\n'를 %c로 출력하면 줄이 바뀐다.
 	printf("소문자 : %c", small);
 
 	return 0;
