b4bd9bdc165a894341ecfb7dd4d107242d7ea236
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-06 14:15:18 +0900

    Create quiz16.c

---
+++ C/Quiz/quiz16.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void print_max(int* arr);
+
+int main()
+{
+	int array[7] = { 4,5,8,1,2,3,7 };
+
+	print_max(array);
+
+	return 0;
+}
+
+void print_max(int* arr)
+{
+	int max = 0;
+
+	for (int i = 0; i < 7; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+
+	printf("가장 큰 값 : %d\n", max);
+}
