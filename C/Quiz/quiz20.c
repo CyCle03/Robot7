2924bc373ca10a2a38f1e25dfe1a28c798c57b5f
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-07 14:49:46 +0900

    Create quiz20.c

---
+++ C/Quiz/quiz20.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[80];
+	int cnt = 0;
+
+	printf("모음의 개수를 셀 영어를 소문자로 입력하세요. : ");
+	fgets(str, 80, stdin);
+
+	for (int i = 0; i < strlen(str) ; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+		{
+			cnt++;
+		}
+	}
+
+	printf("모음은 총 %d개 입니다.\n", cnt);
+
+	return 0;
+}
