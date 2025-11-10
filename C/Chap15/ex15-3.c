580708b7d0470bf14ec8865dd4bd4a9293cd4a0e
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-10 15:54:11 +0900

    Add program to print array of strings

---
+++ C/Chap15/ex15-3.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void print_str(char** pps, int cnt);
+
+int main()
+{
+	//char ptr_arr[4][10];//문자열 배열 상수
+	//char* ptr_ary[4] = { "eagle", "tiger", "lion", "squirrel" };
+	char* ptr_ary[] = { "eagle", "tiger", "lion", "squirrel" };
+	//char** ptr_ary = { "eagle", "tiger", "lion", "squirrel" };//error
+
+	int count = sizeof(ptr_ary)/ sizeof(ptr_ary[0]);//4
+
+	print_str(ptr_ary, count);
+
+	return 0;
+}
+
+void print_str(char** pps, int cnt)
+{
+	for (int i = 0; i < cnt; i++)
+	{
+		printf("%s\n", pps[i]);
+	}
+}
