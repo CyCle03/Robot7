7fde69bedf2eafd87ad274f30701295501ff9139
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-11 10:53:08 +0900

    Enhance memory allocation with error handling
    Added error checking for malloc and improved memory allocation comments.

--- C/Chap16/ex16-1.c
+++ C/Chap16/ex16-1.c
@@ -11,11 +11,29 @@ int main()
 	double* pd;
 	int a, b;
 
-	pi = (int*)malloc(sizeof(int));
+	pi = (int*)malloc(sizeof(int));//malloc(4)
 	printf("Heap\n%p\n", pi);
 	printf("Stack\n%p\n%p\n", &a, &b);
 	printf("String\n%p\n", "hello");
 	printf("Global\n%p\n", &total);
+
+	printf("\n");
+
+	if (pi == NULL)//NULL 0
+	{
+		printf("# 메모리가 부족합니다.\n");
+		exit(1);
+	}
+
+	pd = (double*)malloc(sizeof(double));//malloc(8)
+
+	*pi = 10;//stack 메모리가 아닌
+	*pd = 3.4;//Heap 메모리 공간에 값 삽입
+
+	printf("%d\t%.2lf\n", *pi, *pd);
+
+	free(pi);
+	free(pd);
 
 	return 0;
 }
