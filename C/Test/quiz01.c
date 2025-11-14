b116b939515efbe6944209c1911b588157f012fe
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-14 14:26:04 +0900

    Create quiz01.c

---
+++ C/Test/quiz01.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int width = 20, height = 10;
+	int area = 0;
+
+	area = width * height / 2;
+
+	printf("넓이는 %d입니다.\n", area);
+
+	return 0;
+}
