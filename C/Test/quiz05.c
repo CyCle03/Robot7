ce35de068705651394c0410176d22bfbad273621
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-14 14:28:30 +0900

    Create quiz05.c

---
+++ C/Test/quiz05.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	for (int i = 9; i > 1; i--)
+	{
+		for (int j = 1; j < 10; j++)
+		{
+			printf("%d * %d = %d\n", i, j, i * j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
