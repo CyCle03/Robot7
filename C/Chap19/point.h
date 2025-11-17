7317906ae824ba752195bff1a0c5047a3b185259
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-17 12:24:14 +0900

    Create point.h

---
+++ C/Chap19/point.h
@@ -0,0 +1,18 @@
+//#pragma once
+
+//header 파일 하는 역할 : 함수 정의, 구조체 정의
+
+#ifndef _POINT_H_
+#define _POINT_H_
+
+//void myprint(); //함수헤더
+
+typedef struct
+{
+	int x;
+	int y;
+} Point;
+
+extern Point Gpoint;
+
+#endif
