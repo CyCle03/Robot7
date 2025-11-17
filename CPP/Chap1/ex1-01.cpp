5a19b87fec1bf09c54c7a97e77857cf92a1bda3e
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-17 16:29:49 +0900

    Create ex1-01.cpp

---
+++ CPP/Chap1/ex1-01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "당신의 첫 번째 이름을 입력하세요 : ";
+    std::string name; //string 변수 선언
+    std::cin >> name;
+
+    //인사말 작성
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
