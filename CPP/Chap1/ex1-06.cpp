d18fd82d901034783dcfd47e8b09249785a351fc
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-18 11:37:06 +0900

    Create ex1-06.cpp

---
+++ CPP/Chap1/ex1-06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    bool b1 = true;
+    bool b2 = false;
+
+    std::cout << "b1 : " << b1 << std::endl;
+    std::cout << "b2 : " << b2 << std::endl;
+
+    return 0;
+}
