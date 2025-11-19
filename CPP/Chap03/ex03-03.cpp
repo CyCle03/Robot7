b14aadb2de0aaa0480914c310c089535df6b44de
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-19 14:32:10 +0900

    Create ex03-03.cpp

---
+++ CPP/Chap03/ex03-03.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+class Circle
+{
+public:
+    //1. 멤버 변수
+    int radius;
+    std::string color;
+    //2. 생성자
+    Circle()    //default 생성자
+    {
+        this->radius = 1;
+        std::cout << "반지름이 " << this->radius << "인 원 생성" << std::endl;
+    }
+    Circle(int r)
+    {
+        this->radius = r;
+        std::cout << "반지름이 " << this->radius << "인 원 생성" << std::endl;
+    }
+        Circle(int r, std::string c)
+    {
+        this->radius = r;
+        color = c;
+        std::cout << "반지름이 " << this->radius << ", 색이 " << c << "인 원 생성" << std::endl;
+    }
+
+    double getArea()
+    {
+        return 3.14*radius*radius;
+    }
+    //3. 멤버 메소드
+};
+
+int main()
+{
+    using namespace std;
+
+    Circle donut;
+    Circle pizza(30);
+    Circle tire(10, "Red");
+
+    return 0;
+}
