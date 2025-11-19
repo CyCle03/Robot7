d7455c211d4c7e358c5e3c09ac474ee7bddec631
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-19 15:19:21 +0900

    Update ex03-04.cpp

--- CPP/Chap03/ex03-04.cpp
+++ CPP/Chap03/ex03-04.cpp
@@ -6,14 +6,21 @@ class Circle
 public:
     //1. 멤버 변수
     int radius;
+    std::string color;
     //2. 생성자
-    Circle():radius(1)//초기화 변수
+    Circle():radius(1), color("WHITE")//초기화 변수
     {
-        std::cout << "반지름이 " << this->radius << "인 원 생성" << std::endl;
+        std::cout << "반지름이 " << this->radius << ", 색상이 " << color << "인 원 생성" << std::endl;
     }
-    Circle(int r):radius(r)
+
+    Circle(int r):radius(r), color("WHITE")
     {
-        std::cout << "반지름이 " << this->radius << "인 원 생성" << std::endl;
+        std::cout << "반지름이 " << this->radius <<  ", 색상이 " << color << "인 원 생성" << std::endl;
+    }
+
+    Circle(int r, std::string c):radius(r), color(c)
+    {
+        std::cout << "반지름이 " << this->radius << ", 색이 " << c << "인 원 생성" << std::endl;
     }
 
     double getArea()
@@ -29,6 +36,7 @@ int main()
 
     Circle donut;
     Circle pizza(30);
+    Circle tire(10, "RED");
 
     return 0;
 }
