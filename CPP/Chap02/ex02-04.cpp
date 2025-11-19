9ec5990b094d86fc065bd947b02434d330439167
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-19 10:33:01 +0900

    Create ex02-04.cpp

---
+++ CPP/Chap02/ex02-04.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "이름을 입력하세요 : ";
+    string name;
+    cin >> name;
+
+    cout << "이름은 " << name << "입니다." << endl;
+
+    return 0;
+}
