06ed0c2cd106a619d1f7534967f28ba70f14202d
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-20 14:48:11 +0900

    Update ex03-09.cpp

--- CPP/Chap03/ex03-09.cpp
+++ CPP/Chap03/ex03-09.cpp
@@ -6,9 +6,11 @@ class PrivateAccessError
 private:
     int a;
     PrivateAccessError();
+    void f();
 public:
     int b;
     PrivateAccessError(int x);
+    void g();
 };
 
 PrivateAccessError::PrivateAccessError()
@@ -23,10 +25,26 @@ PrivateAccessError::PrivateAccessError(int x)
     b = x;
 }
 
+void PrivateAccessError::f()
+{
+    a = 5;
+    b = 5;
+}
+
+void PrivateAccessError::g()
+{
+    a = 6;
+    b = 6;
+}
+
 int main()
 {
-    //PrivateAccessError objA; //default 생성자가 private기 때문에 에러
+    //PrivateAccessError objA; //default 생성자가 private기 때문에 접근 불가
     PrivateAccessError objB(100);
+    //objB.a = 10; // a가 private이기 때문에 접근 불가
+    objB.b = 20;
+    //objB.f(); // 마찬가지로 f()가 private기 때문에 접근 불가
+    objB.g();
 
     return 0;
 }
