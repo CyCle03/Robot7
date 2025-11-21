c7f4830a74deb040e9dca36c9f80f27435f0b277
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-21 15:29:36 +0900

    Create oop10.cpp

---
+++ CPP/Chap04/oop10.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+
+class Circle
+{
+public:
+    void draw()
+    {
+        std::cout << "원을 그리다." << std::endl;
+    }
+};
+
+int main()
+{
+    //동적할당
+    int* p = new int;
+    *p = 5;
+    delete p;
+
+    //배열의 동적할당
+    int* p2 = new int[3];
+    p2[0] = 0;
+    p2[1] = 1;
+    delete [] p;
+
+    //c++11 smartpointer
+    std::unique_ptr<int> p3 = std::make_unique<int>(5);
+    std::unique_ptr<Circle> p4 = std::make_unique<Circle>();
+    p4->draw();
+    
+    return 0;
+}
