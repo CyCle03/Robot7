9b2b4440008aa7b5778718b0263bfdca1d7ad6c5
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-24 12:25:35 +0900

    Create oop16.cpp

---
+++ CPP/Chap04/oop16.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+#include <vector>
+using namespace std;
+
+class Hero
+{
+public:
+    virtual void attack()
+    {
+        cout << "기본 공격" << endl;
+    }
+};
+
+class Knight : public Hero
+{
+public:
+    void attack() override
+    {
+        cout << "기사 공격" << endl;
+    }
+};
+
+class Wizard : public Hero
+{
+public:
+    void attack() override
+    {
+        cout << "마법사 공격" << endl;
+    }
+
+};
+
+int main()
+{
+    vector<Hero*> list;
+
+    list.push_back(new Hero());
+    list.push_back(new Knight());
+    list.push_back(new Wizard());
+
+    for(Hero* p : list)
+    {
+        p->attack();
+    }
+
+    
+    return 0;
+}
