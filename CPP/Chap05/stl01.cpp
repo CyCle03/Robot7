dfe739af55f3ee7d1429403269bb355a5007add3
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-24 15:15:15 +0900

    Create stl01.cpp

---
+++ CPP/Chap05/stl01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+void mySwap(T &a, T &b)
+{
+    T tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int main()
+{
+    int a = 4, b = 5;
+    mySwap(a, b);
+
+    cout << "a: " << a << " b: " << b << endl;
+    return 0;
+}
