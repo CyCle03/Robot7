343a7213a649055c97c061f6c03be0cbe702dac0
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-11-25 11:53:18 +0900

    Update stl04.cpp

--- CPP/Chap05/stl04.cpp
+++ CPP/Chap05/stl04.cpp
@@ -7,11 +7,12 @@ int main()
     map<string, string> dic;
 
     dic.insert(make_pair("love", "사랑"));
-    //dic["love"] = "사랑";
+    dic["world"] = "세계";
 
-    string kor = dic["love"];
-    //string kor = dic.at("love");
+    string kor1 = dic["love"];
+    string kor2 = dic.at("world");
 
-    cout << kor <<endl;
+    cout << kor1 <<endl;
+    cout << kor2 <<endl;
 
 }
