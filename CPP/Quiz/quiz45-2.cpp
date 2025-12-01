61d61f73259f7c00fbb15ff929bcfc1ebbb3a5dd
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-01 10:03:24 +0900

    Create quiz45-2.cpp

---
+++ CPP/Quiz/quiz45-2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+using namespace std;
+
+class Hero
+{
+protected:
+    string name;
+    string weapon;
+public:
+    Hero(string name = "영웅", string weapon = "막대기")
+    {
+        this->name = name;
+        this->weapon = weapon;
+    }
+
+    virtual void attack()
+    {
+        cout << name << "이(가) " << weapon << "(으)로 공격" << endl;
+    }
+};
+
+class Knight : public Hero
+{
+public:
+    Knight() : Hero("기사", "검")
+    {
+    }
+    void attack() override
+    {
+        cout << name << "이(가) " << weapon << "(으)로 공격" << endl;
+    }
+};
+
+class Archer : public Hero
+{
+public:
+    Archer() : Hero("궁수", "활")
+    {
+    }
+    void attack() override
+    {
+        cout << name << "이(가) " << weapon << "(으)로 공격" << endl;
+    }
+};
+
+class Wizard : public Hero
+{
+public:
+    Wizard() : Hero("마법사", "지팡이")
+    {
+    }
+    void attack() override
+    {
+        cout << name << "이(가) " << weapon << "(으)로 공격" << endl;
+    }
+};
+
+int main()
+{
+    unique_ptr<Hero> knight = make_unique<Knight>();
+    unique_ptr<Hero> archer = make_unique<Archer>();
+    unique_ptr<Hero> wizard = make_unique<Wizard>();
+
+    knight->attack();
+    archer->attack();
+    wizard->attack();
+    
+    return 0;
+}
