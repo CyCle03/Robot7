6fd7454f72b07e25d62298fe53ebbfd32b9e331f
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-02 12:35:32 +0900

    Create static_test_03.cpp

---
+++ CPP/Chap06/static_test_03.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class PersonError
+{
+private:
+    int money;
+    static inline int staticMoney = 0;
+public:
+    // static int getMoney()
+    // {
+    //     return money; // Error: cannot access non-static member 'money' from a static context
+    // }
+    static int getStaticMoney()
+    {
+        return staticMoney; // Correct: accessing static member 'staticMoney'
+    }
+    void setMoney(int money)
+    {
+        this->money = money; // Correct: accessing non-static member 'money' through 'this' pointer
+        staticMoney += money; // Correct: accessing static member 'staticMoney'
+    }
+    int getMoney()
+    {
+        return money; // Correct: accessing non-static member 'money'
+    }
+};
+
+int main()
+{
+    PersonError pr;
+    pr.setMoney(10000);
+    cout << "Money: " << pr.getMoney() << endl;
+
+    return 0;
+}
