bd42c12e8a31d816557c28dcef8fdf262266c8ea
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-03 12:33:21 +0900

    Create ex09-04.cpp

---
+++ CPP/Chap09/ex09-04.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+    virtual void draw()
+    {
+        cout << "--shape--";
+    }
+};
+
+class Circle : public Shape
+{
+public:
+    virtual void draw()
+    {
+        Shape::draw();
+        cout << "Circle"<< endl;
+    }
+};
+
+int main() 
+{
+    Circle circle;
+    Shape* pShape = &circle;
+
+    pShape->draw();
+    pShape->Shape::draw(); // Calls base class draw method directly
+
+    return 0;
+}
