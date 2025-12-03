c6376911370e9d4e3c63bed592f5cfe7f417d716
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-03 11:37:34 +0900

    Create ex09-02-1.cpp

---
+++ CPP/Chap09/ex09-02-1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Shape
+{
+public:
+    virtual void draw() 
+    {
+        cout << "Shape을 그린다." << endl;
+    }
+};
+class Circle : public Shape
+{
+public:
+    void draw() override { cout << "Circle을 그린다." << endl; }
+};
+class Rect : public Shape
+{
+public:
+    void draw() override { cout << "Rect를 그린다." << endl; }
+};
+class Line : public Shape
+{
+public:
+    void draw() override { cout << "Line을 그린다." << endl; }
+};
+
+void paint(Shape* p)
+{
+    p->draw();
+}
+
+int main()
+{
+    paint(new Circle());
+    paint(new Rect());
+    paint(new Line());
+
+    return 0;
+}
