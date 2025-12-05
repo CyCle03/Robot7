e4b8754617c3dee08d59a28087360624e11204e0
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-05 12:30:23 +0900

    Create ex02.py

---
+++ Python/chap01/ex02.py
@@ -0,0 +1,9 @@
+#두 정수 width, height를 입력 받아 
+#사각형의 넓이를 구하는 프로그램을 작성해 보세요.
+
+width = int(input('밑변의 길이를 입력하세요 : '))
+height = int(input('높이의 길이를 입력하세요 : '))
+
+area = width * height
+
+print("밑변이", width, "높이가", height, "인 사각형의 넓이는", area, "입니다.")
