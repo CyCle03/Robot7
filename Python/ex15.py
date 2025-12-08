98173373a05c77ccee1680d49377c26c179505d2
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-08 16:11:23 +0900

    Update ex15.py

--- Python/ex15.py
+++ Python/ex15.py
@@ -2,8 +2,8 @@ import time #파이썬 내부 모듈
 
 number = 0
 
-target_tick = time.time() + 5
+target_tick = time.time() + 3
 
 while time.time() < target_tick:
     number += 1
-    print('5초 동안 {}번 반복했습니다.'.format(number))
+    print('3초 동안 {}번 반복했습니다.'.format(number))
