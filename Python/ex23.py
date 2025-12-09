20fa7538daf6b55941fc0c593b307856b9ed7087
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-09 12:34:19 +0900

    Create ex23.py

---
+++ Python/ex23.py
@@ -0,0 +1,10 @@
+import sys
+
+def print_n_times(value, n = 2, end='\n', sep = ' ', 
+                  flush = False, file=sys.stdout):
+    #n번 반복
+    for i in range(n):
+        print(value)
+
+print_n_times('안녕하세요')
+print_n_times("반갑습니다", 3)
