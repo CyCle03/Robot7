673157e257786b1da10614d63d8da1dd12f873cb
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-09 11:34:55 +0900

    Update ex22_2.py

--- Python/ex22_2.py
+++ Python/ex22_2.py
@@ -3,3 +3,8 @@ print(listtwo)
 
 listthree = [i * 3 for i in range(1, 10, 1)]
 print(listthree)
+
+words = ['apple', 'kiwi', 'banana', 'grape', 'pear']
+
+long_word = [ word for word in words if len(word) >= 5 ]
+print(long_word)
