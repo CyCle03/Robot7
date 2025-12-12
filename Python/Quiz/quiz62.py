2e44ef306ab75fd7bc376b85094ca936a7b24ec4
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-12 10:26:58 +0900

    Add Student class with methods for management

---
+++ Python/Quiz/quiz62.py
@@ -0,0 +1,36 @@
+class Student:
+    def __init__(self, id, name, pnumber):
+        self.__id = id
+        self.__name = name
+        self.__pnumber = pnumber
+
+    def study(self):
+        print('공부를 합니다.')
+    
+    def getId(self):
+        return self.__id
+    def setId(self, id):
+        self.__id = id
+
+    def getName(self):
+        return self.__name
+    def setName(self, name):
+        self.__name = name
+
+    def getPnumber(self):
+        return self.__pnumber
+    def setPnumber(self, pnumber):
+        self.__pnumber = pnumber
+
+    def printInfo(self):
+        print(f'{self.__id}, {self.__name}, {self.__pnumber}')
+
+student1 = Student(1, '홍길동', '010-1111-1111')
+print(f'{student1.getId()}, {student1.getName()}, {student1.getPnumber()}')
+
+student1.setName('이순신')
+student1.setId(2)
+student1.setPnumber('010-1234-5678')
+student1.printInfo()
+
+student1.study()
