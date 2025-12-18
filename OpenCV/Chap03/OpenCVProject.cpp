efec4fc8555c3dd0f42ba3aa6cd4ce1fddc97157
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-18 16:23:54 +0900

    Update OpenCVProject.cpp

--- OpenCV/Chap03/OpenCVProject.cpp
+++ OpenCV/Chap03/OpenCVProject.cpp
@@ -1,9 +1,11 @@
 //#include "opencv2/opencv.hpp"
 extern void show1();//이 파일 바깥에 있어.
+extern void show2();
 #include <iostream>
 
 int main()
 {
     std::cout << "OpneCV!\n";
     show1();
+    show2();
 }
