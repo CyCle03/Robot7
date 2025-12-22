e8d314211e840925053870e188db911e6d735007
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-22 15:44:35 +0900

    Update OpenCVProject.cpp

--- OpenCV/OpenCVProject.cpp
+++ OpenCV/OpenCVProject.cpp
@@ -1,11 +1,49 @@
 //#include "opencv2/opencv.hpp"
 extern void show1();//이 파일 바깥에 있어.
 extern void show2();
+extern void show3();
+extern void show4();
+extern void show5();
+extern void show6();
+extern void show7();
+extern void show8();
+extern void show9();
+extern void show10();
+extern void show11();
+extern void show12();
+extern void show13();
+extern void show14();
+extern void show15();
+extern void show16();
+extern void show17();
+extern void show18();
+extern void show19();
+extern void show20();
+extern void show21();
 #include <iostream>
 
 int main()
 {
     std::cout << "OpneCV!\n";
     //show1();
-    show2();
+    //show2();
+    //show3();
+    //show4();
+    //show5();
+    //show6();
+    //show7();
+    //show8();
+    //show9();
+    //show10();
+    //show11();
+    //show12();
+    //show13();
+    //show14();
+    //show15();
+    //show16();
+    //show17();
+    //show18();
+    //show19();
+    //show20();
+    show21();
 }
