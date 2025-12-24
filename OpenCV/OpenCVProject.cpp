ab2736be5853effc1718b0d8d594bb8f2c31bbf4
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-24 10:04:29 +0900

    Update OpenCVProject.cpp

--- OpenCV/OpenCVProject.cpp
+++ OpenCV/OpenCVProject.cpp
@@ -22,6 +22,19 @@ extern void show20();
 extern void show21();
 extern void show22();
 extern void show23();
+extern void show24();
+extern void show25();
+extern void show26();
+extern void show27();
+extern void show28();
+extern void show29();
+extern void show30();
+extern void show31();
+extern void show32();
+extern void show33();
+extern void show34();
+
+extern void yaku1();
 #include <iostream>
 
 int main()
@@ -49,5 +62,18 @@ int main()
     //show20();
     //show21();
     //show22();
-    show23();
+    //show23();
+    //show24();
+    //show25();
+    //show26();
+    //show27();
+    //show28();
+    //show29();
+    //show30();
+    //show31();
+    //show32();
+    //show33();
+    show34();
+    
+    //yaku1();
 }
