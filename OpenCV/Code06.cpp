7e230b1bd758d54352f91d6bb21a65cdcc72f35f
CyCle03 <79369598+CyCle03@users.noreply.github.com>
2025-12-24 10:36:55 +0900

    Update Code06.cpp

--- OpenCV/Code06.cpp
+++ OpenCV/Code06.cpp
@@ -52,3 +52,20 @@ void show35()
 	cv::waitKey();
 	cv::destroyAllWindows();
 }
+
+void show36()
+{
+	float s;
+	std::cout << "크기 변환 할 값(s)을 입력하세요.(배로 늘어납니다.): ";
+	std::cin >> s;
+	cv::Mat src = cv::imread("tekapo.bmp");
+	cv::Mat move = cv::Mat_<float>({ 2, 3 }, { s, 0.0f, 0.0f, 0.0f, s, 0.0f });
+	cv::Mat dst;
+
+	cv::warpAffine(src, dst, move, cv::Size(src.cols * s,  src.rows * s));
+	cv::imshow("SRC", src);
+	cv::imshow("DST", dst);
+
+	cv::waitKey();
+	cv::destroyAllWindows();
+}
